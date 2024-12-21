55e36c06a6bb0725910641fc97ba81a867c248bc
Juan Nícolas <juannicolasdev@gmail.com>
2024-12-20 23:29:02 -0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,72 @@
+const filmes = [
+  ['images/saw.png', 'Jogos Mortais', '1º LUGAR',  'Muitos filmes de terror lançados antigamente, se consagraram de alguma forma por trazer coisas novas ao gênero do horror, e com Jogos Mortais não foi diferente. Jogos Mortais 1 se transformou em um clássico do terror por seu roteiro intrigante e direção cirúrgica de James Wan, que também foi um dos idealizadores do projeto, o filme quase praticamente inaugurou essa temática de Body Horror, sendo criado até mesmo um novo subgênero do terror especialmente para colocar o filme, seus sucessores e outros com a mesma temática, o Torture Porn. O filme possui um roteiro muito bom e direto, consegue fazer uma boa dualidade com os personagens presos no jogo do John Kramer (JigSaw), e os detetives tentando desvendar o caso. E o que realmente pega nesse filme é o GRANDE plot twist no final que simplesmente estava na sua cara e não percebemos até o último minuto do filme (literalmente). Até mesmo pro fã de horror que não curte muito filmes sangrentos pode ser indicado, porque de toda a saga, esse continua sendo o mais "leve".', '5 estrelas', '4 de Fevereiro de 2005', '1h 44min', 'Terror, Suspense', 'Torture Porn, Body Horror', 'James Wan', 'James Wan, Leigh Whannell', 'images/cenas/saw/Frame 1.png', 'images/cenas/saw/Frame 2.png', 'images/cenas/saw/Frame 3.png'],
+
+  ['images/exorcist.png', 'O Exorcista', '2º LUGAR', 'Esse filme pra mim, é o melhor filme de possessão demoníaca que existe atualmente. Acho muito hilário que minha geração praticamente cresceu sendo atormentado pela Regan, vulgo menina do exorcista, porque era justamente uma foto dela que tava em todo lugar na internet nos anos 2000, se tinha um vídeo com um jumpscare no meio, pode ter certeza que veriamos ela, inclusive no mais famoso jogo, Labirinto do Terror. Eu nem sempre fui muito fã de filmes longos, mas quando se está imerso em toda ambientação que o filme traz, você nem percebe a hora passar, e 2 horas passam voando. Graças a essa ambientação que o filme cria, ele fica livre para seguir o próprio tempo sem parecer chato ou muito longo para o espectador. E então no decorrer do filme vemos simplesmente tudo, desde a origem do demônio Pazuzu, como ele chegou até a Regan, o processo da possessão e do exorcismo, o desenrolar da história, e acredite ou não, temos tempo até mesmo para conhecer cada personagem importante do filme. Umas das muitas coisas memoráveis que esse filme nos trouxe, foi sua música tema, que apesar de tão icônica, no filme quando é tocada, a mãe da Regan está simplesmente andando pela rua, acho que essa colocação não foi de forma aleatória, na minha opinião, como leva um certo tempo até os eventos sobrenaturais começam, o filme nos apresenta essa trilha em uma cena relativamente simples, para dizer que tem algo grande vindo, para não ficarmos muito relaxados enquanto tudo está normal e propositalmente causar um desconforto.', '5 estrelas', '29 de julho de 1974', '2h 01min', 'Terror', 'Sobrenatural', 'William Friedkin', 'William Peter Blatty', 'images/cenas/exorcist/Frame 1.png', 'images/cenas/exorcist/Frame 2.png', 'images/cenas/exorcist/Frame 3.png'],
+
+  ['images/rec.png', 'REC', '3º LUGAR', 'E agora temos um dos meus Found Footages preferidos. Apesar de parecer, esse filme não é sobre zumbis, é parecido com a situação dos monstros de The Last Of Us kkkkk. Bom, nesse filme acompanhamos uma jornalista que está fazendo um programa noturno em Barcelona, onde no episódio de hoje ela iria está acompanhando o corpo de bombeiros, aparentemente tinha tudo para ser mais uma matéria tranquila para o jornal local, mas infelizmente não acaba como o planejado, ao invés disso, nos deparamos como vírus, gripe, quarentena, zumbis(?). Meu amor pelos filmes Found Footage se dá pela imersão que eles fazem, sendo um filme em primeira pessoa, ter seu campo de visão limitado como o do personagem da tela, são experiências únicas quando visto pela primeira vez. Nesse filme, não só me senti muito imerso, como me senti de fato assistindo a um jornal onde deu tudo errado. Pra mim o ponto chave desse filme, é como ele trabalhou seus sentimentos desde o início, antes de todo o terror começar, o filme realmente nos dá essa experiência de assistir a uma reportagem leve no corpo de bombeiros, onde conhecemos o quartel, os próprios bombeiros e rotina do trabalho noturno, o filme se permite brincar com esse contexto mostrando o jantar dos bombeiros e suas piadas, inclusive eles convidam a jornalista e seu cameraman para comerem com eles, com tudo isso, é criado uma atmosfera de conforto, e o fato de estar de noite só reforça isso, porque o objetivo desse quadro do jornal, é justamente mostrar essa vida noturna, talvez pra mostrar que esse turno pode ser tão bom e movimentado quanto o matutino e vespertino. Com esse sentimento de conforto criado e fixado na memória, a ação começa com os bombeiros indo atender um chamado em um prédio, e aí acontece de tudo um pouco, mas pode ter certeza que com toda essa tensão que é vivida durante o filme, seu único pensamento é querer voltar pro início do filme e querer morar lá de tão confortável que estava, mas não se enganem, o filme como todo é muito bom, e tem um final muito bom que de fato abre um contexto histórico para entender o porque disso está acontecendo, e com esse contexto aberto, veio as sequências para explorar um pouco mais esse universo.', '5 estrelas', '14 de novembro de 2008', '1h 20min', 'Terror', 'Found Footage', 'Paco Plaza, Jaume Balagueró', 'Roteiro Jaume Balagueró, Paco Plaza', 'images/cenas/rec/Frame 1.png', 'images/cenas/rec/Frame 2.png', 'images/cenas/rec/Frame 3.png'],
+
+  ['images/blair.png', 'A Bruxa de Blair', '4º LUGAR - MENÇÃO HONROSA', 'Apesar de não ser o primeiro Found Footage, é considerado o pai (ou seria mãe?) e precursor desse subgênero, e devo dizer que estão muito bem representados. A parte mais incrível desse filme, é que seu terror vai além das telas do cinema, com histórias para contar da produção, gravação, lançamento e pós-lançamento. Mas sobre o filme em si, saber que foi gravado praticamente sem roteiro, e apenas com os atores sendo jogados na floresta, deixa tudo mais interessante. O filme traz uma gravação encontrada de um grupo de cineastas que desapareceram anos atrás enquanto gravavam um documentário sobre uma lenda local. O filme se consagra como um dos maiores clássicos do terror, não só por sua história, mas como ele foi vendido ao público, na época do lançamento lá em 1999, esse filme foi lançado dizendo que realmente eram gravações encontradas de pessoas que estavam perdidas a muito tempo, e para reforçar isso, os nomes dos personagem e dos atores são os mesmos, o que acredite, gerou problemas depois kkkkk. Com esse marketing muito bem feito, e para uma época onde a internet não era acessível para todos, e pelo fato de ser a primeira obra a fazer essa mesclagem entre ficção e realidade, esse filme foi bastante assistido, e não deixou de traumatizar muitas pessoas com sua história. Anos mais tarde, Atividade Paranormal usou a mesma jogada, e deu certo. Isso são eventos que apenas o Found Footage consegue criar por trazer essa proximidade com o espectador, por isso é meu subgênero preferido.', '5 estrelas', '1 de outubro de 1999', '1h 18min', 'Terror', 'Found Footage', 'Daniel Myrick, Eduardo Sanchez', 'Roteiro Daniel Myrick, Eduardo Sanchez', 'images/cenas/blair/Frame 1.png', 'images/cenas/blair/Frame 2.png', 'images/cenas/blair/Frame 3.png'],
+
+  ['images/coraline.png', 'Coraline', '5º LUGAR - MENÇÃO HONROSA', 'Em breve!', '5 estrelas', '13 de fevereiro de 2009', '1h 40min', 'Animação, Fantasia, Terror', 'Gótico, Sobrenatural', 'Henry Selick', 'Henry Selick', 'images/cenas/coraline/Frame 1.png', 'images/cenas/coraline/Frame 2.png', 'images/cenas/coraline/Frame 3.png'],
+]
+
+filmes.forEach(filme => {
+  const div = document.createElement('div');
+  div.id = 'par';
+  div.setAttribute('data-bs-toggle', 'modal');
+  div.setAttribute('data-bs-target', '#myModal');
+
+  div.innerHTML = `
+      <img src="${filme[0]}" alt="">
+      <h3>${filme[1]}</h3>
+  `;
+
+  div.addEventListener('click', () => {
+    document.querySelector('#exampleModalLabel').innerText = `${filme[1]}`
+  })
+
+  div.addEventListener('click', () => {
+      document.querySelector('.modal-body').innerHTML = `
+          
+            <div id="carouselExampleIndicators" class="carousel slide">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="${filme[11]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="${filme[12]}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                  <img src="${filme[13]}" class="d-block w-100" alt="...">
+                </div>
+              </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+          <h2>${filme[2]}</h2>
+          <p>${filme[3]}</p>
+          <strong>NOTA: ${filme[4]}</strong>
+          <br><br>
+          <h4>Ficha Tecnica:</h4>
+          <p>Lançamento: ${filme[5]}</p>
+          <p>Duração: ${filme[6]}</p>
+          <p>Genero: ${filme[7]}</p>
+          <p>Subgenero: ${filme[8]}</p>
+          <p>Direção: ${filme[9]}</p>
+          <p>Roteiro: ${filme[10]}</p>
+          `;
+  });
+
+  document.querySelector('#mural').appendChild(div);
+});
